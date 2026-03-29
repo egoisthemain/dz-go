55f0a9f238af7f35fad2ca7083872580b34fea64
Egormain <egoisthemain@gmail.com>
2026-03-30 01:32:59 +0300

    3-bin: modified constructors

--- 3-bin/main.go
+++ 3-bin/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Bin struct {
 	id        string
@@ -13,9 +16,28 @@ type BinList struct {
 	bins []Bin
 }
 
-func newBin(id string, private bool, name string) {}
+func newBin(id string, private bool, name string) (*Bin, error) {
+	if id == "" {
+		return nil, errors.New("wrong id")
+	}
+	if name == "" {
+		return nil, errors.New("wrong name")
+	}
+	bin := Bin{
+		id:        id,
+		private:   private,
+		createdAt: time.Now(),
+		name:      name,
+	}
+	return &bin, nil
+}
 
-func newBinList(bin Bin) {}
+func newBinList(bin Bin) (*BinList, error) {
+	binList := BinList{
+		bins: []Bin{bin},
+	}
+	return &binList, nil
+}
 
 func main() {
 
