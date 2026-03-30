f84fb0069f99adbbbafccd3ed8cfbf0f6edc674e
Egormain <egoisthemain@gmail.com>
2026-03-30 23:21:15 +0300

    edit read func and SaveBin

--- 3-bin/files/files.go
+++ 3-bin/files/files.go
@@ -3,10 +3,19 @@ package files
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 func WriteFile() {
 
+}
+
+func ReadFile(filename string) ([]byte, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return []byte{}, err
+	}
+	return data, nil
 }
 
 func ReadJsonFile(filename string) bool {
@@ -14,7 +23,12 @@ func ReadJsonFile(filename string) bool {
 	if err != nil {
 		return false
 	}
-	return json.Valid(data)
+	ext := filepath.Ext(filename)
+	if ext == ".json" && json.Valid(data) {
+		return true
+	} else {
+		return false
+	}
 }
 
 func main() {
